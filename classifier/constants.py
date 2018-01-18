fd3035b8098bfae8fa52cea92d4a96d00e20a553
aylliote <elliot.hofman@sondeos.com.ar>
2018-01-18 17:28:17 +0100

    Update constants.py

--- classifier/constants.py
+++ classifier/constants.py
@@ -1,41 +1,8 @@
 import re
 import json
 
-## Constants Twitter crawler
-
-badwords = ['peligroso','peligrosa','peligrosos','peligrosas','triste','tristeza','tristes', 'peina','lastima','lastimado','lastimada','lastimados','lastimadas' 'malicima',
-            'tarde frio','demorar','arrepiento','arrepintio','decepcionados','decepcionado','negativos','demoro','tontos',
-            'tontas','tonta','tonto','carajo','desinteres','inrecomendable','desaparecio','costosos','costoso','romper la pelota',
-            'dificiles','dificil', 'complicado','complicadisimo ','pesadisimo','pesado','pesados',
-            'precios caros','precio caro','ni empedo','lamentablemente','rabia','lastima','lamento','imposible',
-            'asquerosas','asquerosos','asqueroso','bronca','verguenza','inmaduros','inmadurez','ridiculos','aguantate boludo',
-            'insoportables','insoportable','orto','traidor','traidores','garca','garcas','impostor','impostores','frio',
-            'malisimo','malisima','malisimas','malisimos','odio','odios','mentiroso','mentirosos','mentirosa','mentirosas',
-            'pelotudeces','chorro','chorros','ladrones','ladron','boludos','catastrofe','bajon','ratas de mierda',
-            'rata de mierda', 'forro', 'forra','malparido','malparidos','decepcion','decepcionado','decepcionada','hdp',
-            'choteada','choteadas','ni en pedo','ni borracho','dejen de joder','jodete','me estan jodiendo','me estas jodiendo',
-            'me estas cagando','me estan cagando','basura','cagadas','harto de','chupar la pija','pito','choteada', 'no me gusto',
-            'muy malo','muy mala', 'muy malas', 'muy malos','mande a la mierda','mandan a la mierda','verga','rompe el culo',
-            'chupaculo', "chupaculos",'harto de','no doy mas','hinchar las pelotas','me cago en la concha de tu madre','cagada',
-            'hincha las pelotas','chupenmela','la puto que te re contra pario', 'andate a la re concha de la lora', 'andate a la mierda',
-            'hijueputa', 'rompe las bolas','gil','giles','nazi del orto','forro','forra', 'me saca', 'en la loma del culo',
-            'en la loma del orto','cagada','me cago','anden a la mierda','rompe el orto','chupame la pija','chupaculo',
-            'chupame el pito','no es muy bueno', 'no me gusto', 'imbancable', 'chupamedias', 'zarpado','por el orto',
-            'toqueton', 'zarpados', 'conchudo', 'conchuda','conchudos', 'conchudas','mala leche', 'ortiba', 'me cago en la conche de tu hermana',
-            'forro', 'forra','yearsa','tarado','tarados','chulengo','chulengos','berreta','ratas de mierda','rata de mierda','cacuija',
-            'buitre','cacuijas','buitres','mamerto','marmetos','guacala','cagador','pastrulo','verreta','puteadas','torpe','me canse de',
-            'callate','al pedo','baboso','la concha de tu madre','la puta que ta pario', 'la re concha','putos','puto','puta','putas',
-            'hijos de las mil putas','la concha de la lora','mucho que mejorar','solo te puede entrener si', 'chupenla', 'chupala','ni borraho',
-            'cagon', 'marica','a la mierda' 'maricones', 'maricon', 'cagones', 'cheto', 'cheta', 'chetos', 'chetas', 'boludeando','boludear',
-            'hijo de puta','pelotudo','chorro','sos una mierda','boludo del orto', 'zorra','gil','pelotudos','boludos','gilipolla']
-
-goodWords = ['vamooo','amo','amor','precioso','preciosa','preciosos','preciosas','satisfacion','viva amor','enamorado','enamorada',
-            'enamorados','enamoradas','agradecidas','agradecidos','agradecido','agradecida','cumplidora','honesta','felices','satisfecho',
-             'satisfechos','contentos','contento','recomendable','espectacular','piola','buenos','buenas','buenisimo',
-             'buenisima','buenisimos']
-
 ## Constants importation Data
-pathsData = { 'SENSACINE_SERIES' : "Classifier/data/sensaCineSerie.txt",
+""" pathsData = { 'SENSACINE_SERIES' : "Classifier/data/sensaCineSerie.txt",
               'SENSACINE_MOVIES' : "Classifier/data/sensaCineMovie.txt",
               'PEDIDOS_YA' : "Classifier/data/pedidosYa.txt",
               'TRIP_ADVISOR_HOTEL' : "Classifier/data/tripAdvisorHotel.txt",
@@ -48,13 +15,13 @@ pathsData = { 'SENSACINE_SERIES' : "Classifier/data/sensaCineSerie.txt",
               'GOOD_TWEETS' : "Classifier/data/goodTweets.txt",
               'TASS_DATASET' : "Classifier/data/tassTweets.txt",
               'MERCADOLIBRE_POS' : "Classifier/data/MercadoPos.txt",
-              'MERCADOLIBRE_NEG' : "Classifier/data/MercadoNeg.txt" }
+              'MERCADOLIBRE_NEG' : "Classifier/data/MercadoNeg.txt" } """
 
 MERCADO_RATIO = 50000
 dataDelimiter = '__dataDelimiter__'
 
 ## Constants preProcessing
-
+path = './'
 dirtyReps = re.compile(r'([^lL0])\1{1,}')
 dirtySpaces = re.compile(r'(\.|,|:|;|!|\?|\[|\]|\(|\))[A-Za-z0-9]+')
 dirtyK = re.compile('[^o]k')
@@ -62,9 +29,11 @@ dirtyJaja = re.compile(r'[ja]{5,}')
 dirtyJeje = re.compile(r'[je]{5,}')
 numbers = re.compile(r'[0-9]+')
 uglySeparator = 'THIS-IS-A-SEPARATOR'
-pathExpressions = 'Classifier/data/expressions.txt' ## XXX : to fill
+pathExpressions = path_package + '/data/expressions.txt' ## XXX : to fill
 exps = set([e.split()[0] for e in open(pathExpressions, encoding='utf8').readlines()])
-pathConjug = "C:/Users/ellio/test.json" ## XXX : to fill
+pathConjug = path_package + '/data/dictConjug.json' ## XXX : to fill
+pathCities = path_package + '/data/Cities.txt' ## XXX : to fill
+pathCountries = path_package + '/data/Countries.txt' ## XXX : to fill
 
 with open(pathConjug, 'r') as f:
     dictConjug = json.load(f)
@@ -92,9 +61,10 @@ neutralWords = set(['mueble', 'producto','pelicula','poner','senor','venderme','
 
 ## Constants Classifier
 best_params = {'f__k': 70000, 'v__max_features': 200000, 'v__ngram_range': (1, 4), 'c__alpha': 0.01}
-pathModel = 'sentiment_pipeline.pkl'
+pathModel = path_package + '/model/sentiment_pipeline.pkl'
 
 ## Constants Prediction
 decayRate = 8
-GOOD_BABY = 'Classifier/img/good_baby.jpg'
-BAD_BABY = 'Classifier/img/bad_baby.jpg'
+
+GOOD_BABY = path_package + '/img/good_baby.jpg'
+BAD_BABY = path_package + '/img/bad_baby.jpg'
